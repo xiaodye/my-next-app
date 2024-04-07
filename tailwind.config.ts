a653f7ff172447a613fcf87d739e02d5cb82c2a1
lansen_i <lansen_i@didiglobal.com>
2024-04-07 15:50:51 +0800

    feat: tailwindcss 正则匹配和类名排序配置

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,9 +1,16 @@
 import type { Config } from "tailwindcss";
 import { fontFamily } from "tailwindcss/defaultTheme";
+import { nextui } from "@nextui-org/react";
 
 const config = {
   darkMode: ["class"],
-  content: ["./pages/**/*.{ts,tsx}", "./components/**/*.{ts,tsx}", "./app/**/*.{ts,tsx}", "./src/**/*.{ts,tsx}"],
+  content: [
+    "./pages/**/*.{ts,tsx}",
+    "./components/**/*.{ts,tsx}",
+    "./app/**/*.{ts,tsx}",
+    "./src/**/*.{ts,tsx}",
+    "./node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}",
+  ],
   prefix: "",
   theme: {
     container: {
@@ -73,7 +80,7 @@ const config = {
       },
     },
   },
-  plugins: [require("tailwindcss-animate")],
+  plugins: [require("tailwindcss-animate"), nextui],
 } satisfies Config;
 
 export default config;
